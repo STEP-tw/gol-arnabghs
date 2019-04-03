2f2a50a058ce8aee2e0bafa8ce8ba7e0fbe7f5ac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-03 18:01:50 +0000

    Deploy STEP-tw/gol-arnabghs to github.com/STEP-tw/gol-arnabghs.git:gh-pages

---
+++ precache-manifest.a715483660494f6882a3db1ef574700d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23ef8e51740a346e0072",
+    "url": "/gol-arnabghs/static/js/runtime~main.23ef8e51.js"
+  },
+  {
+    "revision": "d1c036634f9208f9ae35",
+    "url": "/gol-arnabghs/static/js/main.d1c03663.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/gol-arnabghs/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "d1c036634f9208f9ae35",
+    "url": "/gol-arnabghs/static/css/main.051c869c.chunk.css"
+  },
+  {
+    "revision": "df35648bfe54c27251b7720e4db9540f",
+    "url": "/gol-arnabghs/index.html"
+  }
+];
